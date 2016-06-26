a29995f97d157cf5116630520bbe677e6b2af52a
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2016-06-26 16:47:09 +0200

    2

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# SCUMM_Launcher_OSX
-Launcher Script to Build an APP out of SCUMM
+# ScummVM Launcher
+## Build an simple APP with SCUMMVM an Automator
 
+### Get ScummVM from
+[ScummVM](http://scummvm.org/downloads/ "ScummVM Download")
+
+
+### Get GameID from
+[GameID](https://www.scummvm.org/compatibility/ "GameID")
--- scumm.sh
+++ scumm.sh
@@ -3,24 +3,21 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin export PATH
 # launcher script for scummvm games
 #######################################
 # ".app Name"
-APP_NAME="Maniac Mainson"
+APP_NAME="Flight of the Amazon Queen"
 
 # folder where the game is stored
-GAME_DIRECTORY="maniac"
+GAME_DIRECTORY="queen"
 
 # scummvm game id
-GAME_ID="maniac"
+GAME_ID="queen"
 #######################################
 
 # where scummvm is stored
 SCUMMVM="/ScummVM.app/Contents/MacOS/scummvm"
 
-# logging for debugging
-#exec > >(tee /tmp/scumm.txt) 2>&1
-
 # Get the working directory the the app is stored
 BIN=$(ps x | grep -I "${APP_NAME}" | grep -v grep | cut -c 28- )
-DIR=$(dirname "$BIN")
+DIR=$(dirname "${BIN}")
 cd "${DIR}"
 
 # change to the app directory
@@ -29,16 +26,12 @@ CUR_DIR=$(pwd)
 # escape spaces in path
 #CUR_DIR=$(printf %q "$CUR_DIR")
 
-SCUMM_CONFIG="${CUR_DIR}/configfile"
+SCUMM_CONFIG="${CUR_DIR}/scumm.conf"
 
 SCUMM_PATH="${CUR_DIR}${SCUMMVM}"
 
 GAME_FOLDER="${CUR_DIR}/${GAME_DIRECTORY}"
 
 SAVE_PATH="${GAME_FOLDER}/savegames"
-
-LAUNCHER="${SCUMM_PATH} --config=${SCUMM_CONFIG} --savepath=${SAVE_PATH} -f -p ${GAME_FOLDER} ${GAME_ID}"
-
-#echo "###${LAUNCHER}###"
 
 "$SCUMM_PATH" "--config=${SCUMM_CONFIG}" "--savepath=${SAVE_PATH}" "-f" "-p" "${GAME_FOLDER}" "${GAME_ID}"
